66ffb33a66f22f6019f404b1798fdfd74b94097c
MGSE97 <elektrikar97@gmail.com>
2019-04-14 17:31:21 +0200

    Working with files, Text Files, Simple, Usage, Reading

---
+++ Working with files/Text files/Simple/Usage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Usage
+{
+    public class Program
+    {
+        public static string FileToRead  = "FileToRead.txt";
+        public static string FileToWrite = "FileToWrite{0}.txt";
+
+        static void Main(string[] args)
+        {
+            #region Reading
+                Console.WriteLine("Reading");
+
+                Console.WriteLine("\n\t FileStream method:\n");
+                Console.WriteLine(Read.FileStream());
+
+                Console.WriteLine("\n\t TextReader method:\n");
+                Console.WriteLine(Read.TextReader());
+
+                Console.WriteLine("\n\t FileInfo method:\n");
+                Console.WriteLine(Read.FileInfo());
+
+                Console.WriteLine("\n\t File method:\n");
+                Console.WriteLine(Read.File());
+            #endregion
+        }
+    }
+}
---
+++ Working with files/Text files/Simple/Usage/Read.cs
@@ -0,0 +1,49 @@
+﻿namespace Usage
+{
+    public static class Read
+    {
+        public static string FileStream()
+        {
+            string text = null;
+
+            using (var fileStream = new System.IO.FileStream(Program.FileToRead, System.IO.FileMode.Open))
+                using (var textReader = new System.IO.StreamReader(fileStream))
+                    text = textReader.ReadToEnd();
+
+            return text;
+        }
+
+        public static string File()
+        {
+            string[] lines = System.IO.File.ReadAllLines(Program.FileToRead);
+            if (lines.Length > 0)
+                return string.Join('\n', lines);
+
+            return null;
+        }
+
+        public static string FileInfo()
+        {
+            string text = null;
+
+            var fileInfo = new System.IO.FileInfo(Program.FileToRead);
+
+            if (fileInfo.Exists)
+                using (var fileStream = fileInfo.OpenRead())
+                    using (var textReader = new System.IO.StreamReader(fileStream))
+                        text = textReader.ReadToEnd();
+
+            return text;
+        }
+
+        public static string TextReader()
+        {
+            string text = null;
+
+            using (System.IO.TextReader textReader = new System.IO.StreamReader(Program.FileToRead))
+                text = textReader.ReadToEnd();
+
+            return text;
+        }
+    }
+}
